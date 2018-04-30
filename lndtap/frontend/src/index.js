647bece5e3c9fd2af937077d22bb236880e061cb
Ritesh Kadmawala <ritesh@loanzen.in>
2018-04-30 16:14:53 +0530

    feat(frontend): setup frontend for the app using create-react-app

---
+++ lndtap/frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import './css/index.css';
+import App from './pages/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
